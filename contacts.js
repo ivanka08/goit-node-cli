8127eee59d86d1c56eb1c11baa2798e0ec9d556f
ivanka08 <ivankahudic@gmail.com>
2024-01-17 11:27:06 +0200

    End

--- contacts.js
+++ contacts.js
@@ -1,4 +1,3 @@
-
 import { readFile, writeFile } from "fs/promises";
 import { join } from "path";
 
@@ -13,20 +12,24 @@ async function listContacts() {
 async function getContactById(contactId) {
   const contacts = await listContacts();
   const contact = contacts.find((contact) => contact.id === contactId);
-  return contact;
+  return contact ?? null;
 }
 
 async function removeContact(contactId) {
   const contacts = await listContacts();
-  const contact = contacts.find((contact) => contact.id === contactId);
-  contacts.splice(contacts.indexOf(contact), 1);
-  await writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
-  return contact;
+  const contactIndex = contacts.findIndex((contact) => contact.id === contactId);
+  if (contactIndex !== -1) {
+    contacts.splice(contactIndex, 1);
+    await writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
+    return contacts[contactIndex];
+  } else {
+    return null;
+  }
 }
 
 async function addContact(name, email, phone) {
   const contacts = await listContacts();
-  const contactId = Math.max(...contacts.map((contact) => contact.id)) + 1;
+  const contactId = contacts.length > 0 ? Math.max(...contacts.map((contact) => contact.id)) + 1 : 1;
   const contact = {
     id: contactId,
     name,
